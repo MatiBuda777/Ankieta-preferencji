cc9085cbe9d62fa602ebfe4ebfa736e8ef0c53fb
Mateusz <mateusz.budek@cyfroweszkoly.pl>
2025-03-18 15:42:10 +0100

    App works

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,29 @@
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Ankieta;
 
 public partial class MainWindow : Window
 {
+    string UserName;
+    string Interest;
+    
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void SubmitButton_Click(object? sender, RoutedEventArgs e)
+    {
+        if (TextBoxName.Text != null) UserName = TextBoxName.Text;
+        if (ComboBoxInterest.SelectionBoxItem != null) Interest = ComboBoxInterest.SelectionBoxItem.ToString();
+    }
+    
+    private void ShowButton_Click(object? sender, RoutedEventArgs e)
+    {
+        CheckBox[] CheckBoxes = {CheckBox1, CheckBox2, CheckBox3}; 
+        int numOfYes = CheckBoxes.Count(checkbox => checkbox.IsChecked == true);
+        TextBlockShow.Text = $"{UserName}, zainteresowanie: {Interest}, liczba odpowiedzi na tak: {numOfYes}";
     }
 }
